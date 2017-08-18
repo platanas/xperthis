d5051c6a7b7e399c0dea19f636c902a8a7c52177
platanas <info@platanas.be>
2017-08-18 17:07:10 +0200

    Review design and translation

--- sites/all/modules/webform_protected_downloads/webform-protected-downloads-download-page.tpl.php
+++ sites/all/modules/webform_protected_downloads/webform-protected-downloads-download-page.tpl.php
@@ -1,3 +1,4 @@
+<div class="col-sm-8 no-padding">
 <?php if ($text): ?>
   <?php echo $text; ?>
 <?php endif; ?>
@@ -5,3 +6,4 @@
 <?php if ($files_table): ?>
   <?php echo $files_table; ?>
 <?php endif; ?>
+</div>
